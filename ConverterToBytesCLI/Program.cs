1aa9f41baf422b3fbc6904f31dfd8a68e5592041
rob.brz <robert.brzozowski@ftsgps.com>
2022-01-20 15:12:00 +0100

    Add project files.

---
+++ ConverterToBytesCLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConverterToBytesCLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
